dfe268cc6e3740b0fc3e85ae790b8813a88444c8
Jems Shrestha <jemsstha111@gmail.com>
2026-06-01 01:57:24 +0545

    lecture1

---
+++ scratch.py
@@ -0,0 +1,36 @@
+print("hello world,", "i am jems")
+a=23
+b=24
+print("sum:", a+b)
+c=99.9
+d=None
+print(type(c))
+print(type(d))
+# hi i am comments
+print(a==b)
+e=6
+e+=5
+print(e)
+print(True or False)
+
+# Type conversion, automatic converion
+v=2
+w=3.5
+print(v+w)
+
+# Type casting
+f="2"
+g=5
+print(int(f)+g)
+print(type(f))
+print(type(int(f)))
+
+# input in python
+name=input("Enter your name: ")
+print(name)
+print(type(name),name)
+z=input("enter number: ")
+print(type(int(z)),z)
+
+o=2
+print(o**3)
