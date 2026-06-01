15c6d779222b2590f822982600d8721d46d94c90
Jems Shrestha <jemsstha111@gmail.com>
2026-06-01 22:15:08 +0545

    second lecture done

--- scratch.py
+++ scratch.py
@@ -1,4 +1,4 @@
-print("hello world,", "i am jems")
+'''print("hello world,", "i am jems")
 a=23
 b=24
 print("sum:", a+b)
@@ -34,3 +34,83 @@ print(type(int(z)),z)
 
 o=2
 print(o**3)
+'''
+
+# string and conditional statements
+'''str1= "This is a string.\nwe arw creating it in python"
+print(str1)
+"scape sequence charecters= \n \t"
+"concatination= '+' opearator"
+Str1="hello"
+Str2="world"
+print(Str1+" "+Str2)
+print(len(Str1))'''
+
+
+'''# indexing
+str="Jems Shrestha"
+print(str[0])
+# slicing
+print(str[0:3])
+print(str[ :3])
+print(str[1: ])
+#negative indexing
+print(str[-3:-1])'''
+
+'''#string function
+str="i am studing python"
+print(str.endswith("on"))
+print(str.capitalize())
+print(str.replace("o","a"))
+print(str.replace("python","java"))
+print(str.find("o"))
+print(str.count("o"))'''
+
+'''# qn
+name=input("Enter your name: ")
+print("length of string is: ",len(name))
+p="hi i am $ and is 566$"
+print(p.count("$"))'''
+
+# Condtional Statements
+'''age=21
+if(age>18):
+    print("can vote")
+    print("can drive")
+elif(age==18):
+    print("is 18")
+else:
+    print("age<18")
+
+marks=int(input("Enter your marks: "))
+if(90<marks<=100):
+    print("A+")
+elif(marks>80):
+    print("A")
+elif(marks>70):
+    print("B+")
+elif(marks>60):
+    print("B")
+elif(marks>50):
+    print("C+")
+else:
+    print("Fail")
+
+age=16
+weight=40
+if(age>18):
+    if(weight>25):
+        print("can donate blod")
+else:
+    print("cannot donate blod")'''
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if(a>=b and a>=c):
+    print("first is largest.")
+elif(b>=c):
+    print("second is largest.")
+else:
+    print("third is largest.")
+
